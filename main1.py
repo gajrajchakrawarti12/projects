e4a075d0e106f503bdae58b8de0c46f9187c8b76
Gajraj Chakrawarti <156901405+gajrajchakrawarti12@users.noreply.github.com>
2024-12-19 19:30:12 +0530

    .

--- main1.py
+++ main1.py
@@ -26,7 +26,7 @@ def main(def_args=sys.argv[1:]):
     days_after = args.days_after
     if days_after < 0:
         sys.exit('days_after must not be negative')
-    os.mkdir(directory)
+    # os.mkdir(directory)
     os.chdir(directory)
     run(['git', 'init', '-b', 'main'])
 
